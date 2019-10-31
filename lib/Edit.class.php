4fb1fa60a736af3a17cdb252de7104b1a955f30e
kakuzator <wyarbo0461@yahoo.co.jp>
2019-10-31 13:13:18 +0900

    最新版

--- lib/Edit.class.php
+++ lib/Edit.class.php
@@ -42,17 +42,25 @@ class Edit
     }
 
     public function makeCheckBox($option, $detail)
-    {
+    {//0・1形式にすれば一回のループで可→テンプレート側での対応まで可 あるいはforeach
+		//htmlの文字列を変数に格納する
+		//checked = "checked"
+		//<input hidden value=0 name=check>
+		//<input checkbox value=0 name=check>
+		//チェックボックスは選択されたら値が送られる。
+		//同じnameを指定することでチェックをした際POSTでcheckを送信するときの値が上書かれる
         $checkbox = [];
         for ($i = 0; $i < 7; $i++) { //7つの項目を表示させる
             for ($y = 1; $y < 8; $y++) { //各項目でチェックを行う
-                if ($option[$i]['id'] == $detail['option_id' . $y]) {
+                if ($option[$i]['id'] == $detail['option_id' . $y]) {//foreachを、使う場合keyに+1して処理
                     array_push(
                         $checkbox,
-                        '<label class="option-label"><input class="option" type="checkbox" name="option[]" value=' . $option[$i]['id'] . ' checked>' . $option[$i]['text'] . '</label>'
+                        '<label class="option-label"><input class="option" type="checkbox" name="option[]" value=' . $option[$i]['id'] . ' checked>'
+                            . $option[$i]['text'] . '</label>'
                     );
                 } elseif ($y == 7 && empty($checkbox[($i)])) {
-                    array_push($checkbox, '<label class="option-label"><input class="option" type="checkbox" name="option[]" value=' . $option[$i]['id'] . ' >' . $option[($i)]['text'] . '</label>');
+                    array_push($checkbox, '<label class="option-label"><input class="option" type="checkbox" name="option[]" value=' . $option[$i]['id'] . ' >'
+                        . $option[($i)]['text'] . '</label>');
                 }
             }
         }
